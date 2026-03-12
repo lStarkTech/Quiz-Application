0cfa9e66201499f21cb4e0ecef1cc451f2ce402d
lStarkTech <chiara.l.salvato@gmail.com>
2026-03-12 08:22:53 +0100

    First_version

---
+++ first_program.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from PyQt6.QtGui import QGuiApplication, QIcon
+from PyQt6.QtQml import QQmlApplicationEngine
+
+def trova_file(nome_file):
+    """
+    Se l'app è un .exe, cerca nella cartella segreta _MEIPASS.
+    Altrimenti, cerca nella cartella normale di VS Code.
+    """
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, nome_file)
+    return os.path.join(os.path.abspath("."), nome_file)
+
+app = QGuiApplication(sys.argv)
+app.setWindowIcon(QIcon(trova_file("./images/icon.ico")))
+
+percorso = trova_file("Welcome_page.qml")
+engine = QQmlApplicationEngine()
+engine.quit.connect(app.quit)
+engine.load(percorso)
+if not engine.rootObjects():
+    sys.exit(-1)
+
+sys.exit(app.exec())
